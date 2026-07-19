411bd9fd9b39fd1c3a6835a244052ddf8cca6368
Deep-Kotak <deepmkotak1103@gmail.com>
2026-07-19 15:47:40 +0530

    feat: add vehicle update method

--- app/domain/vehicle.py
+++ app/domain/vehicle.py
@@ -9,14 +9,24 @@ class Vehicle:
         id=None,
     ):
         if price < 0:
-            raise ValueError("Price cannot be negative.")
-
+            raise ValueError("price cannot be negative")
         if quantity < 0:
-            raise ValueError("Quantity cannot be negative.")
-
+            raise ValueError("quantity cannot be negative")
         self.id = id
         self.make = make
         self.model = model
         self.category = category
         self.price = price
         self.quantity = quantity
+
+    def update_details(self, make=None, model=None, category=None, price=None):
+        if price is not None:
+            if price < 0:
+                raise ValueError("price cannot be negative")
+            self.price = price
+        if make is not None:
+            self.make = make
+        if model is not None:
+            self.model = model
+        if category is not None:
+            self.category = category
